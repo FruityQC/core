f8bc1e96ae2325e62a5fce9b335b818e172d263a
Im a Young Developper <61483651+TheYoungDevelopper@users.noreply.github.com>
2022-02-16 11:49:54 -0500

    Initial commit

---
+++ spawn/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+author 'Fruity'
+description 'None'
+
+server_scripts {
+    'server.lua',
+    '@mysql-async/lib/MySQL.lua'
+}
---
+++ spawn/server.lua
@@ -0,0 +1,32 @@
+AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
+    local source = source
+    local identifiers = GetPlayerIdentifiers(source)
+    local steamid
+    local license 
+
+    for k, v in ipairs(identifiers) do 
+        if string.match(v, 'steam:') then 
+            steamid = v
+            print(v)
+        end
+    end
+
+    if not steamid then
+        deferrals.done('Steam est requis.')
+    else --Si steam est ouvert
+       deferrals.done() 
+
+       MySQL.Async.fetchScalar('SELECT 1 FROM user_identifiers WHERE steamid = @steamid', {
+           ['@steamid'] = steamid 
+       }, function(result)
+            if not result then
+                print('STEAM_ID ERR')
+
+                MySQL.Async.execute('INSERT INTO user_identifiers (steamname, steamid, license) VALUES (@steamname, @steamid, @license',
+                {['@steamname'] = GetPlayerName(source), ['@steamid'] = steamid, ['@license'] = license})
+            else
+                print("Steamid OK")
+            end
+        end)
+    end
+end)
